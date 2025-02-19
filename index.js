c7e981077d6ba225c4113b37204d586882617fd1
RamaKrs <ramiroalvarez295@gmail.com>
2025-02-19 20:21:49 -0300

    arreglando puerto y start node index js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import db from './config/db.js'
 
 const app = express()
 
-const port = process.env.port || 4000
+const port = process.env.PORT || 3000
 
 db.authenticate()
     .then(() => console.log('bd conectada'))
