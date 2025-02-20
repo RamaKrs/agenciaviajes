2e7cd4b5c427b2ab8c8bca3809dac9b619782926
RamaKrs <ramiroalvarez295@gmail.com>
2025-02-19 23:08:53 -0300

    b

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 
 // process.env.DATABASE_URL
-const db = new Sequelize(DATABASE_URL, {
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     define: {
         timestamps: false
